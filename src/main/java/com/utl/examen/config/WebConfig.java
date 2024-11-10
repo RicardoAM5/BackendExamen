0e1966636a48ed9c0594f31b45978f66c50b42ef
Ricardo Alonso Medina <ricardoalonsomedina99@gmail.com>
2024-11-09 21:38:40 -0600

    heroku

---
+++ src/main/java/com/utl/examen/config/WebConfig.java
@@ -0,0 +1,15 @@
+package com.utl.examen.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://tourmaline-basbousa-fac2fa.netlify.app/") // URL de tu frontend desplegado
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
+}
